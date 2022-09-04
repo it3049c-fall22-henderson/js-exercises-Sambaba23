7024507746292a31925a77f933ed21f01270d853
Sambaba23 <81830827+Sambaba23@users.noreply.github.com>
2022-09-04 15:13:44 -0400

    completed equlaity tests

--- src/7.equality.js
+++ src/7.equality.js
@@ -2,14 +2,16 @@
 // Example: strictEquality(1, true) => false
 // Example: strictEquality(1, 1) => true
 function strictEquality(val1, val2) {
-  return ;
+  b = val1=== val2
+  return b;
 }
 //equality: compare value but not necessarily the type
 // Example: equality(1, true) => true
 // Example: equality(1, 1) => true
 // Example: equality(1, false) => false
 function equality(val1, val2) {
-  return ;
+  b = val1 == val2
+  return b;
 }
 
 module.exports = {
