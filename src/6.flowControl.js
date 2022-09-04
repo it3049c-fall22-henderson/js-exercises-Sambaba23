a68620fe92c26e6641e015a6d2a6c0361763743e
Sambaba23 <81830827+Sambaba23@users.noreply.github.com>
2022-09-04 14:59:52 -0400

    completed the fizzbuzz function

--- src/6.flowControl.js
+++ src/6.flowControl.js
@@ -7,8 +7,17 @@
  *    if no number was provided or if the value provided wasn't a number (hint: typeof), return false
  */
 function fizzBuzz(num) {
-  
+if (typeof num === 'number'){
+    if (num % 15 == 0) return "fizzbuzz"
+    else if (num % 3 == 0) return "fizz"
+    else if (num% 5 == 0) return "buzz"
+    else return num;
 }
+else return false;
+
+
+}
+
 
 module.exports = {
   fizzBuzz
